6812d1d17122f86a0234a30b9f8b4760251b9c85
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-03-06 16:59:04 +0100

    Add image to article

---
+++ Apps.Zendesk/Models/Requests/ImageRequest.cs
@@ -0,0 +1,18 @@
+﻿using Apps.Zendesk.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Zendesk.Models.Requests
+{
+    public class ImageRequest
+    {
+        [Display("Image")]
+        public FileReference File { get; set; }
+    }
+}
