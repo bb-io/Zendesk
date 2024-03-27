254705c34c4fc6725f49b02b803a0021ed40a1e4
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-03-27 16:10:15 +0100

    Added label actions

---
+++ Apps.Zendesk/Models/Responses/ArticleResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Zendesk.Models.Responses
+{
+    public class ArticleResponse
+    {
+    }
+}
---
+++ Apps.Zendesk/Models/Responses/TranslationWithLabels.cs
@@ -0,0 +1,32 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Apps.Zendesk.Models.Responses
+{
+    public class TranslationWithLabels : Translation
+    {
+        [Display("Labels")]
+        public IEnumerable<string> Labels { get; set; }
+
+        public TranslationWithLabels(Translation article, IEnumerable<string> labels)
+        {
+            Id = article.Id;
+            HtmlUrl = article.HtmlUrl;
+            Draft = article.Draft;
+            Outdated = article.Outdated;
+            CreatedAt = article.CreatedAt;
+            UpdatedAt = article.UpdatedAt;
+            Title = article.Title;
+            Locale = article.Locale;
+            Body = article.Body;
+            SourceId = article.SourceId;
+
+            Labels = labels;
+        }
+    }
+}
