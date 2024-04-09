30ab87907e691aee9f0e76e77b0ad53a5bf00d99
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 15:59:25 +0300

    added categories

--- Apps.Zendesk/ZendeskApplication.cs
+++ Apps.Zendesk/ZendeskApplication.cs
@@ -2,14 +2,21 @@
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Zendesk;
 
-public class ZendeskApplication : BaseInvocable, IApplication
+public class ZendeskApplication : BaseInvocable, IApplication, ICategoryProvider
 {
     private string _name;
     private readonly Dictionary<Type, object> _typesInstances;
 
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.Cms, ApplicationCategory.CustomerSupport];
+        set { }
+    }
+    
     public ZendeskApplication(InvocationContext invocationContext) : base(invocationContext)
     {
         _name = "Zendesk";
