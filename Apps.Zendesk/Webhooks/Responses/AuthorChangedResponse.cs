7230dbc81f918c5ab9a036da0577cb3c6a0ca70d
RiabushenkoA <ariabushenko@blackbird.io>
2025-03-17 20:04:50 +0200

    Added filter for webhook

--- Apps.Zendesk/Webhooks/Responses/AuthorChangedResponse.cs
+++ Apps.Zendesk/Webhooks/Responses/AuthorChangedResponse.cs
@@ -6,5 +6,19 @@ public class AuthorChangedResponse : ArticleResponse
 {
     [Display("New author ID")]
     public string AuthorId { get; set; }
-        
+
+    [Display("Category")]
+    public string CategoryId { get; set; }
+
+    [Display("Locale")]
+    public string Locale { get; set; }
+
+    [Display("Section")]
+    public string SectionId { get; set; }
+
+    [Display("Title")]
+    public string Title { get; set; }
+
+    [Display("Labels")]
+    public List<string> Labels { get; set; }
 }
--- Apps.Zendesk/Webhooks/WebhookList.cs
+++ Apps.Zendesk/Webhooks/WebhookList.cs
@@ -350,17 +350,50 @@ public class WebhookList : BaseInvocable
     }
 
     [Webhook("On article author changed", typeof(ArticleAuthorChangedHandler), Description = "On article author changed")]
-    public async Task<WebhookResponse<AuthorChangedResponse>> ArticleAuthorChangedHandler(WebhookRequest webhookRequest)
+    public async Task<WebhookResponse<AuthorChangedResponse>> ArticleAuthorChangedHandler(WebhookRequest webhookRequest, [WebhookParameter] ArticlePublishedInputParameter input)
     {
         var data = JsonConvert.DeserializeObject<ArticlePayloadTemplate<AuthorEvent>>(webhookRequest.Body.ToString());
-        if (data is null) { throw new InvalidCastException(nameof(webhookRequest.Body)); }
+        if (data is null)
+        {
+            throw new InvalidCastException(nameof(webhookRequest.Body));
+        }
+
+        if (input.BrandId != null && input.BrandId == data.Detail.BrandId)
+        {
+            return new WebhookResponse<AuthorChangedResponse>
+            {
+                HttpResponseMessage = null,
+                ReceivedWebhookRequestType = WebhookRequestType.Preflight,
+                Result = null
+            };
+        }
+
+        if (input.AccountId != null && input.AccountId == data.AccountId.ToString())
+        {
+            return new WebhookResponse<AuthorChangedResponse>
+            {
+                HttpResponseMessage = null,
+                ReceivedWebhookRequestType = WebhookRequestType.Preflight,
+                Result = null
+            };
+        }
+
+        var id = data.Detail.Id;
+        var request = new ZendeskRequest($"/api/v2/help_center/articles/{id}", Method.Get);
+        var response = await Client.ExecuteWithHandling<SingleArticle>(request);
+        var article = response.Article;
+
         return new WebhookResponse<AuthorChangedResponse>
         {
             HttpResponseMessage = null,
             Result = new AuthorChangedResponse
             {
                 ArticleId = data.Detail.Id,
-                AuthorId = data.Event.Current
+                AuthorId = article.AuthorId,
+                Title = article.Title,
+                Locale = article.SourceLocale,
+                SectionId = article.SectionId,
+                Labels = article.Labels?.ToList() ?? new List<string>()
             }
         };
     }
