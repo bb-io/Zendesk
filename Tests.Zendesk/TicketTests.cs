0ea486753d4bbd8745a5d0e5036f14be9789b296
RiabushenkoA <ariabushenko@blackbird.io>
2025-07-07 20:06:41 +0300

    Added action 'Search tickets'

---
+++ Tests.Zendesk/TicketTests.cs
@@ -0,0 +1,30 @@
+﻿using Apps.Zendesk.Actions;
+using Apps.Zendesk.Models.Requests;
+using System.Text.Json;
+using ZendeskTests.Base;
+
+namespace Tests.Zendesk
+{
+    [TestClass]
+    public class TicketTests : TestBase
+    {
+        [TestMethod]
+        public async Task ListTickets_works()
+        {
+            var actions = new TicketActions(InvocationContext);
+
+            var result = await actions.SearchTickets(new SearchTicketsRequest
+            {
+                Status = "open",
+            });
+            Console.WriteLine($"Total tickets found: {result.Tickets.Count()}");
+            foreach (var ticket in result.Tickets)
+            {
+                Console.WriteLine($"Ticket ID: {ticket.Id}, Subject: {ticket.Subject}, Status: {ticket.Status}");
+            }
+            var json = JsonSerializer.Serialize(result.Tickets, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+            Console.WriteLine(json);
+            Assert.IsNotNull(result);
+        }
+    }
+}
