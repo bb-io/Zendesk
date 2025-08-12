d237ff354a6c47f9ce6fea2a83072e12e9f4b101
RiabushenkoA <ariabushenko@blackbird.io>
2025-08-12 13:13:36 +0300

    Unit tests

---
+++ Tests.Zendesk/TicketTests.cs
@@ -0,0 +1,49 @@
+﻿using Apps.Zendesk.Actions;
+using Apps.Zendesk.Models.Identifiers;
+using Apps.Zendesk.Models.Requests;
+using System.Text.Json;
+using ZendeskTests.Base;
+
+namespace Tests.Zendesk
+{
+    [TestClass]
+    public class TicketTests : TestBase
+    {
+        [TestMethod]
+        public async Task SearchTickets_works()
+        {
+            var actions = new TicketActions(InvocationContext);
+
+            var result = await actions.SearchTickets(new SearchTicketsRequest
+            {
+                //Status = "open",
+                //Priority = "normal"
+            });
+            Console.WriteLine($"Total tickets found: {result.Tickets.Count()}");
+            foreach (var ticket in result.Tickets)
+            {
+                Console.WriteLine($"Ticket ID: {ticket.Id}, Subject: {ticket.Subject}, Status: {ticket.Status}");
+            }
+            var json = JsonSerializer.Serialize(result.Tickets, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+            Console.WriteLine(json);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetTicketComments_IsSuccess()
+        {
+            var action = new TicketActions(InvocationContext);
+
+            var article = new TicketIdentifier
+            {
+                Id = "32"
+            };
+
+            var response = await action.GetTicketComments(article);
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(response, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(json);
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
