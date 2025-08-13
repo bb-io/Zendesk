62d650034176ad84a82cec7e00f91f6684b569c9
RiabushenkoA <ariabushenko@blackbird.io>
2025-08-13 15:19:48 +0300

    Added filters to event 'On ticket comment created'

--- Apps.Zendesk/Webhooks/TicketWebhooks.cs
+++ Apps.Zendesk/Webhooks/TicketWebhooks.cs
@@ -1,4 +1,5 @@
-﻿using Apps.Zendesk.Webhooks.Handlers.TicketHandlers;
+﻿using Apps.Zendesk.Models.Requests;
+using Apps.Zendesk.Webhooks.Handlers.TicketHandlers;
 using Apps.Zendesk.Webhooks.Payload;
 using Apps.Zendesk.Webhooks.Responses;
 using Blackbird.Applications.Sdk.Common;
@@ -19,10 +20,54 @@ namespace Apps.Zendesk.Webhooks
         }
 
         [Webhook("On ticket comment created", typeof(TicketCommentCreatedHandler), Description = "On ticket comment created")]
-        public async Task<WebhookResponse<TicketCommentCreatedEventDto>> OnTicketCommentCreatedHandler(WebhookRequest webhookRequest)
+        public async Task<WebhookResponse<TicketCommentCreatedEventDto>> OnTicketCommentCreatedHandler(WebhookRequest webhookRequest,
+            [WebhookParameter] TicketCommentFiltersInput? input)
         {
             var data = JsonConvert.DeserializeObject<TicketCommentCreatedWebhook>(webhookRequest.Body.ToString());
             if (data is null) { throw new InvalidCastException(nameof(webhookRequest.Body)); }
+
+            if (!string.IsNullOrWhiteSpace(input?.IgnoreIfBodyContains))
+            {
+                var body = data.Event?.Comment?.Body ?? string.Empty;
+                if (body.IndexOf(input.IgnoreIfBodyContains, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return new WebhookResponse<TicketCommentCreatedEventDto>
+                    {
+                        HttpResponseMessage = null,
+                        ReceivedWebhookRequestType = WebhookRequestType.Preflight,
+                        Result = null
+                    };
+                }
+            }
+
+            if (input?.OnlyPublic == true)
+            {
+                var isPublic = data.Event?.Comment?.IsPublic ?? false;
+                if (!isPublic)
+                {
+                    return new WebhookResponse<TicketCommentCreatedEventDto>
+                    {
+                        HttpResponseMessage = null,
+                        ReceivedWebhookRequestType = WebhookRequestType.Preflight,
+                        Result = null
+                    };
+                }
+            }
+
+            if (input?.IgnoreIfViaWebService == true)
+            {
+                var channel = data.Detail?.Via?.Channel;
+                if (string.Equals(channel, "web_service", StringComparison.OrdinalIgnoreCase))
+                {
+                    return new WebhookResponse<TicketCommentCreatedEventDto>
+                    {
+                        HttpResponseMessage = null,
+                        ReceivedWebhookRequestType = WebhookRequestType.Preflight,
+                        Result = null
+                    };
+                }
+            }
+
             return new WebhookResponse<TicketCommentCreatedEventDto>
             {
                 HttpResponseMessage = null,
