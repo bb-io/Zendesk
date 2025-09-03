eca52e8b7e484c34ba472f5ad54faca37b196b3a
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-09-03 11:07:09 +0300

    add RefreshToken support and refactor OAuth2TokenService - add a few missing CredNames

--- Apps.Zendesk/Auth/OAuth2/OAuth2TokenService.cs
+++ Apps.Zendesk/Auth/OAuth2/OAuth2TokenService.cs
@@ -1,26 +1,35 @@
-﻿using System.Text.Json;
+﻿using Newtonsoft.Json;
+using Apps.Zendesk.Constants;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
 namespace Apps.Zendesk.Auth.OAuth2;
 
-public class OAuth2TokenService : BaseInvocable, IOAuth2TokenService
-{
-    private static string TokenUrl = "";
+public class OAuth2TokenService : BaseInvocable, IOAuth2TokenService {
+    private string TokenUrl = "";
 
-    public OAuth2TokenService(InvocationContext invocationContext) : base(invocationContext)
-    {
+    public OAuth2TokenService(InvocationContext invocationContext) : base(invocationContext) { }
+
+    public bool IsRefreshToken(Dictionary<string, string> values) {
+        var expiresAt = DateTime.Parse(values[CredNames.ExpiresAt]);
+        return DateTime.UtcNow > expiresAt;
     }
 
-    public bool IsRefreshToken(Dictionary<string, string> values)
-    {
-        return false;
-    }
+    public async Task<Dictionary<string, string>> RefreshToken(Dictionary<string, string> values, CancellationToken cancellationToken) {
+        TokenUrl = $"{new Uri(values["api_endpoint"]).GetLeftPart(UriPartial.Authority).TrimEnd('/')}/oauth/tokens";
+        
+        var parameters = new Dictionary<string, string> {
+            { "grant_type", "refresh_token" },
+            { "client_id", ApplicationConstants.ClientId },
+            { "client_secret", ApplicationConstants.ClientSecret },
+            { "refresh_token", values[CredNames.RefreshToken] },
+            { "expires_in", "3600" }
+        };
 
-    public async Task<Dictionary<string, string>> RefreshToken(Dictionary<string, string> values, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
+        var dictionary = await ExecuteTokenRequest(parameters, cancellationToken);
+        AddExpiresAt(dictionary);
+        return dictionary;
     }
 
     public async Task<Dictionary<string, string?>> RequestToken(
@@ -30,34 +39,43 @@ public class OAuth2TokenService : BaseInvocable, IOAuth2TokenService
         CancellationToken cancellationToken)
     {
         TokenUrl = $"{new Uri(values["api_endpoint"]).GetLeftPart(UriPartial.Authority).TrimEnd('/')}/oauth/tokens";
-        const string grant_type = "authorization_code";
+        string redirectUri = $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/AuthorizationCode";
 
-        var bodyParameters = new Dictionary<string, string>
-        {
-            { "grant_type", grant_type },
+        var bodyParameters = new Dictionary<string, string> {
+            { "grant_type", "authorization_code" },
             { "client_id", ApplicationConstants.ClientId },
             { "client_secret", ApplicationConstants.ClientSecret },
-            { "redirect_uri", $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/AuthorizationCode" },
+            { "redirect_uri", redirectUri },
             { "scope", ApplicationConstants.Scope },
             { "code", code }
         };
-        return await RequestToken(bodyParameters, cancellationToken);
+
+        var dictionary = await ExecuteTokenRequest(bodyParameters, cancellationToken);
+        AddExpiresAt(dictionary);
+        return dictionary;
     }
 
-    public Task RevokeToken(Dictionary<string, string> values)
-    {
+    public Task RevokeToken(Dictionary<string, string> values) {
         throw new NotImplementedException();
     }
 
-    private async Task<Dictionary<string, string>> RequestToken(Dictionary<string, string> bodyParameters, CancellationToken cancellationToken)
-    {
-        var utcNow = DateTime.UtcNow;
-        using HttpClient httpClient = new HttpClient();
-        using var httpContent = new FormUrlEncodedContent(bodyParameters);
-        using var response = await httpClient.PostAsync(TokenUrl, httpContent, cancellationToken);
-        var responseContent = await response.Content.ReadAsStringAsync();
-        var resultDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseContent)?.ToDictionary(r => r.Key, r => r.Value?.ToString())
-                               ?? throw new InvalidOperationException($"Invalid response content: {responseContent}");
-        return resultDictionary;
+    private async Task<Dictionary<string, string>> ExecuteTokenRequest(Dictionary<string, string> parameters,
+        CancellationToken cancellationToken) {
+        using var client = new HttpClient();
+        using var content = new FormUrlEncodedContent(parameters);
+        using var response = await client.PostAsync(TokenUrl, content, cancellationToken);
+
+        var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
+        if (!response.IsSuccessStatusCode) {
+            throw new Exception($"Error requesting token: {response.StatusCode} - {responseContent}");
+        }
+
+        return JsonConvert.DeserializeObject<Dictionary<string, string>>(responseContent!)!;
+    }
+
+    private Dictionary<string, string> AddExpiresAt(Dictionary<string, string> dictionary) {
+        var expiresAt = DateTime.UtcNow.AddSeconds(int.Parse(dictionary[CredNames.ExpiresIn]));
+        dictionary.Add(CredNames.ExpiresAt, expiresAt.ToString());
+        return dictionary;
     }
 }
