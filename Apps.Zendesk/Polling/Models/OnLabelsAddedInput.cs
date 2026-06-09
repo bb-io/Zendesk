e7b343e53ec4519d77597b51871255668a35dd9f
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2026-06-09 12:48:02 +0300

    Added 'Deep search' option to 'On labels added to articles' event

--- Apps.Zendesk/Polling/Models/OnLabelsAddedInput.cs
+++ Apps.Zendesk/Polling/Models/OnLabelsAddedInput.cs
@@ -1,4 +1,5 @@
 using Apps.Zendesk.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Zendesk.Polling.Models;
@@ -7,4 +8,9 @@ public class OnLabelsAddedInput
 {
     [DataSource(typeof(LabelNameDataHandler))]
     public IEnumerable<string> Labels { get; set; }
+
+    [Display("Deep search",
+        Description = "When enabled, queries articles directly by label name in addition to the incremental feed. " +
+                      "Use this if label changes are not triggering the event on your Zendesk instance.")]
+    public bool? DeepSearch { get; set; }
 }
